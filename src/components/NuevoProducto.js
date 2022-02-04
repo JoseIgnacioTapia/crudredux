3bfdacafd08b2c2355d992044e73f2c177c48f11
JoseIgnacioTapia <josignaciot@gmail.com>
2022-02-04 15:54:33 -0300

    Defining the action and using useDispatch

--- src/components/NuevoProducto.js
+++ src/components/NuevoProducto.js
@@ -1,4 +1,27 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+// Actions de Redux
+import { crearNuevoProductoAction } from '../actions/productoActions';
+
 const NuevoProducto = () => {
+  // Utilizar useDispatch y te crea una función
+  const dispatch = useDispatch();
+
+  // Mandar llamar el action de productoAction
+  const agregarProducto = () => dispatch(crearNuevoProductoAction());
+
+  // Cuando el usuario haga submit
+  const submitNuevoProducto = e => {
+    e.preventDefault();
+
+    // Validar formulario
+
+    // Si no hay errores
+
+    // Crear en nuevo producto
+    agregarProducto();
+  };
+
   return (
     <div className="row justify-content-center">
       <div className="col-md-8">
@@ -8,7 +31,7 @@ const NuevoProducto = () => {
               Agregar Nuevo Producto
             </h2>
 
-            <form>
+            <form onSubmit={submitNuevoProducto}>
               <div className="form-group">
                 <label>Nombre Producto</label>
                 <input
